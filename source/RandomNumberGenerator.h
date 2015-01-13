e63dd566091a922e88348676838ec56720085e59
Wotwell <Wjotwell@ucdavis.edu>
2015-01-13 15:27:32 -0800

    Update RandomNumberGenerator.h

--- source/RandomNumberGenerator.h
+++ source/RandomNumberGenerator.h
@@ -7,19 +7,19 @@
 
 class CRandomNumberGenerator{
     protected:
-		/**
-		 * This is the seed that generates the upper half of the random number.
-		 */
+        /**
+         * This is the seed that generates the upper half of the random number.
+         */
         unsigned int DRandomSeedHigh;
         /**
          * This is the seed that generates the lower half of the random number.
          */
         unsigned int DRandomSeedLow;
-        
+
     public:
-		/**
-		 * The default constructor just initializes DRandomSeedHigh and DRandomSeedLow to fixed hex values.
-		 */
+        /**
+         * The default constructor just initializes DRandomSeedHigh and DRandomSeedLow to fixed hex values.
+         */
         CRandomNumberGenerator(){
             DRandomSeedHigh = 0x01234567;
             DRandomSeedLow = 0x89ABCDEF;
@@ -40,17 +40,17 @@ class CRandomNumberGenerator{
                 DRandomSeedLow = low;   
             }
         };
-        
+
         /**
          * Changes the value of DRandomHigh and DrandomLow.
          * 
          * Returns an int where DRandomSeedHigh affects the upper 16 bits and DRandomlow affects all 32 bits
          */
         unsigned int Random(){
-			/* 
-			 * multiplies the lower 16 digits by a set number (different for low and high)
-			 * adds this to the upper 16 digits shifted right by 16 bit
-			 */
+            /* 
+             * multiplies the lower 16 digits by a set number (different for low and high)
+             * adds this to the upper 16 digits shifted right by 16 bit
+             */
             DRandomSeedHigh = 36969 * (DRandomSeedHigh & 65535) + (DRandomSeedHigh >> 16);
             DRandomSeedLow = 18000 * (DRandomSeedLow & 65535) + (DRandomSeedLow >> 16);
             return (DRandomSeedHigh << 16) + DRandomSeedLow;
